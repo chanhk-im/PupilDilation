08cbe065d59e22344b7f72069ff6cafa8ba1be7d
chanhk-im <cfasd1875@gmail.com>
2026-03-23 02:06:57 +0900

    first commit

---
+++ src/main/java/com/chanhk/pupildilation/PupilDilationApplication.java
@@ -0,0 +1,13 @@
+package com.chanhk.pupildilation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupilDilationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PupilDilationApplication.class, args);
+    }
+
+}
