0ad9ff02e9ef97b99fde985ea75cdbec58a3651f
chanhk-im <cfasd1875@gmail.com>
2026-03-31 23:49:26 +0900

    feat: user entity 구현

---
+++ src/main/java/com/chanhk/pupildilation/user/domain/User.java
@@ -0,0 +1,43 @@
+package com.chanhk.pupildilation.user.domain;
+
+import com.chanhk.pupildilation.global.BaseEntity;
+import com.chanhk.pupildilation.global.common.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User extends BaseEntity {
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public void changePassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+}
