02329f1ad9b729180c07ff30deb651e33f1a0e1c
--unset <--unset>
2026-04-01 17:22:38 +0900

    fix[#22]: @Email 대신 regx 이용한 @Pattern으로 변경

--- src/main/java/com/chanhk/pupildilation/auth/dto/request/ClubSignupRequest.java
+++ src/main/java/com/chanhk/pupildilation/auth/dto/request/ClubSignupRequest.java
@@ -1,10 +1,15 @@
 package com.chanhk.pupildilation.auth.dto.request;
 
-import jakarta.validation.constraints.Email;
+import com.chanhk.pupildilation.global.common.Constants;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ClubSignupRequest(
-        @NotBlank @Email String email,
+        @NotBlank
+        @Pattern(
+                regexp = Constants.EMAIL_REGX_STRING,
+                message = Constants.EMAIL_VALID_MESSAGE)
+        String email,
         @NotBlank String password,
         @NotBlank String clubName,
         @NotBlank String description
